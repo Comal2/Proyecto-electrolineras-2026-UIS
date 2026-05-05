483cda6378141711b55a56d2a4202c6e79337cf0
Breyner Villamizar <breyneralvilla17@gmail.com>
2026-05-05 18:39:40 -0500

    Update Menu.py

--- codigo/Menu.py
+++ codigo/Menu.py
@@ -10,60 +10,6 @@ import simulacion         # <- import programs
 import modelo_ml
 #import estadisticas
 
-<<<<<<< Updated upstream
-"""El submenu dentro de la op. 1"""
-
-def submenu_mapa_grafos_simulacion():                  # <- definimos la fun que llamamos
-    while True:
-        print("\n=== Mapa, grafo y simulaciones ===")
-        print("1. Cargar mapa y grafo")
-        print("2. Simular recorridos")
-        print("3. Buscar mejor ruta a electrolinera")
-        print("4. Volver al menu principal")
-        sub_opcion = int(input("Elige una opcion: "))
-        
-        match(sub_opcion):
-            case 1:
-                print("\nMostrando mapa y grafo")
-                grafo.main()                           # <- llamamos el programa que importamos
-            case 2:
-                print("\nSimulando recorrido...")
-                simulacion.main() 
-            case 3:
-                print("\nMostrando mejor ruta...")
-                modelo_ml.main()
-            case 4:
-                print("\nVolviendo al menu")
-                break
-            case _:
-                print("Opcion invalida")
-
-
-"""Tendremos nuestro menu principal"""
-
-while True:
-    print("\n=== MENÚ PRINCIPAL ===")
-    print("1. Ver mapa y grafo")  
-    print("2. Ver estadisticas de uso")
-    print("3. Modelo predictivo de nuevas electrolineras")
-    print("4. Salir")
-    opcion = int(input("\nElige una opción: "))
-    
-    match(opcion):
-        case 1:
-            submenu_mapa_grafos_simulacion()                     # <- Llamamos la fun que definimos
-        case 2:
-            print("\nMostrando estadisticas...")
-            #estadisticas.main()
-        case 3:
-            print("\n=== Modelo para nuevas Electrolineras ===")
-            modelo_ml.main()
-        case 4:
-            print("\nSaliendo... ")
-            break 
-        case _:
-            print("Opcion no valida")
-=======
 import sys
 import os
 sys.path.insert(0, os.path.dirname(__file__))
@@ -124,5 +70,4 @@ def main():
                 break 
             case _:
                 print("Opcion no valida")
->>>>>>> Stashed changes
 
