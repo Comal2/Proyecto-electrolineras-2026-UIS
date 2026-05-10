b365a904596ccdac9071cbc7a9fabdd9fe104714
Breyner Villamizar <breyneralvilla17@gmail.com>
2026-05-10 11:53:54 -0500

    Update mapa.py

--- codigo/mapa.py
+++ codigo/mapa.py
@@ -3,13 +3,17 @@ import os
 import folium
 import pandas as pd
 
+#enrutamos los csv y los definimos 
+directorio_mapa = os.path.dirname(os.path.abspath(__file__))
+carpeta_resultados = os.path.join(directorio_mapa, '..', 'resultados')
+ruta_csv_e = os.path.join(directorio_mapa, '..', 'datos', 'electrolineras_con_nodos.csv')
+ruta_csv_p = os.path.join(directorio_mapa, '..', 'datos', 'puntos_con_nodos.csv' )
 
-sys.path.insert(0, os.path.dirname(__file__))
 
+df_e = pd.read_csv(ruta_csv_e)
+df_p = pd.read_csv(ruta_csv_p)
 
-df_e = pd.read_csv("datos/electrolineras_con_nodos.csv")
-df_p = pd.read_csv("datos/puntos_con_nodos.csv")
-
+#creacion del mapa
 mapa = folium.Map(location=[7.1254, -73.1198], zoom_start=13)
 
 for _, fila in df_e.iterrows():
@@ -26,4 +30,6 @@ for _, fila in df_p.iterrows():
         icon=folium.Icon(color='blue')
     ).add_to(mapa)
 
-mapa.save("resultados/mapa_base.html") # codigo de prueba o referencia visual, no refleja el resultado final.
+ruta_html = os.path.join(carpeta_resultados, 'mapa_base.html')
+mapa.save(ruta_html) # codigo de prueba o referencia visual, no refleja el resultado final.
+print(f"Mapa guardado con exito en: {ruta_html}")
