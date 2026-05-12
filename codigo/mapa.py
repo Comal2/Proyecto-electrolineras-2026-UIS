c4dc85130259ac8473bdd386c75c3a1e85a62477
Breyner Villamizar <breyneralvilla17@gmail.com>
2026-05-12 17:36:04 -0500

    Update mapa.py

--- codigo/mapa.py
+++ codigo/mapa.py
@@ -13,13 +13,25 @@ df_p = pd.read_csv(os.path.join(DIRECTORIO, "../datos/puntos_con_nodos.csv"))
 #creacion del mapa
 mapa = folium.Map(location=[7.1254, -73.1198], zoom_start=13)
 
+#creacion nodos 'ELECTROLINERAS'
 for _, fila in df_e.iterrows():
-    folium.Marker(
-        location=[fila['latitud'], fila['longitud']],
-        tooltip=fila['nombre'],
-        icon=folium.Icon(color='green', icon='bolt', prefix='fa')
-    ).add_to(mapa)
+    #Diferenciamos por tipo de carga
+    if fila['tipo_carga'] == 'alta_potencia':   #Electrolineras de alata potencia
+        folium.Marker(
+            location=[fila['latitud'], fila['longitud']],
+            tooltip=fila['nombre'],
+            icon=folium.Icon(color='lightgreen', icon='bolt', prefix='fa')
+        ).add_to(mapa)
 
+    else:
+        #electrolineras de potencia normal
+        folium.Marker(
+            location=[fila['latitud'], fila['longitud']],
+            tooltip=fila['nombre'],
+            icon=folium.Icon(color='green', icon='bolt', prefix='fa')
+        ).add_to(mapa)
+
+#creacion nodos 'PUNTOS REFERENCIA'
 for _, fila in df_p.iterrows():
     folium.Marker(
         location=[fila['latitud'], fila['longitud']],
@@ -27,6 +39,13 @@ for _, fila in df_p.iterrows():
         icon=folium.Icon(color='blue')
     ).add_to(mapa)
 
+#centrar el mapa respecto a los nodos
+mapa.fit_bounds([
+    [df_e['latitud'].min(), df_e['longitud'].min()],
+    [df_p['latitud'].max(), df_p['longitud'].max()]
+])
+
+#guardamos el mapa en los resultados
 mapa.save(os.path.join(DIRECTORIO, "../resultados/mapa_base.html")) # codigo de prueba o referencia visual, no refleja el resultado final.
-#print(f"Mapa guardado con exito en {}") 
+ 
 
